43a6553a4ffc50b07c0e0c06cf5f94426832c754
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-05-16 20:02:59 +0300

    docs: update branch

--- rs-school/task-2.js
+++
@@ -1,8 +0,0 @@
-const { stdin, stdout } = process;
-
-stdout.write('Как тебя зовут?\n');
-stdin.on('data', data => {
-  const dataStringified = data.toString();
-  stdout.write(dataStringified.split('').reverse().join(''));
-  process.exit();
-});
